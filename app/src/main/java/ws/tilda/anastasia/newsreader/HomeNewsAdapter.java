30477421b5002a1b07c74010d681c036807160d9
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-03-07 20:59:24 +0200

    created Adapter and ViewHolder

---
+++ app/src/main/java/ws/tilda/anastasia/newsreader/HomeNewsAdapter.java
@@ -0,0 +1,50 @@
+package ws.tilda.anastasia.newsreader;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ws.tilda.anastasia.newsreader.model.NewsArticle;
+
+
+public class HomeNewsAdapter extends RecyclerView.Adapter<HomeNewsAdapter.HomeNewsViewHolder> {
+    private List<NewsArticle> newsArticles;
+
+    public HomeNewsAdapter(List<NewsArticle> newsArticles) {
+        this.newsArticles = newsArticles;
+    }
+
+    @Override
+    public HomeNewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(HomeNewsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return newsArticles.size();
+    }
+
+    public static class HomeNewsViewHolder extends RecyclerView.ViewHolder {
+        ImageView cardImageView;
+        TextView cardTitleTextView;
+        TextView cardTimeTextView;
+        TextView cardContentTextView;
+
+        public HomeNewsViewHolder(View itemView) {
+            super(itemView);
+            cardImageView = (ImageView) itemView.findViewById(R.id.card_news_image);
+            cardTitleTextView = (TextView) itemView.findViewById(R.id.card_news_title);
+            cardTimeTextView = (TextView) itemView.findViewById(R.id.card_news_time);
+            cardContentTextView = (TextView) itemView.findViewById(R.id.card_news_content);
+        }
+    }
+}
