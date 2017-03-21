26dc94c9043b608f021e5463d35808791bc3a852
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-03-21 15:59:53 +0200

    added attribution to NewsApi.org (using coordinator layout and snackbar)

--- app/src/main/java/ws/tilda/anastasia/newsreader/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/newsreader/MainActivity.java
@@ -1,9 +1,14 @@
 package ws.tilda.anastasia.newsreader;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -14,6 +19,7 @@ import ws.tilda.anastasia.newsreader.networking.NewsAPI;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView newsRecyclerView;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,13 @@ public class MainActivity extends AppCompatActivity {
         newsRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recyclerview);
         newsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
+
         Call<GetArtilcesResponse> call = NewsAPI.getApi().getArticles("reuters", "top");
         call.enqueue(new Callback<GetArtilcesResponse>() {
             @Override
             public void onResponse(Call<GetArtilcesResponse> call, Response<GetArtilcesResponse> response) {
+                showNewsApiSnack();
                 GetArtilcesResponse getArtilcesResponse = response.body();
                 NewsStore.setArticles(getArtilcesResponse.getArticles());
                 Toast.makeText(MainActivity.this, "Response received", Toast.LENGTH_SHORT).show();
@@ -41,4 +50,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void showNewsApiSnack() {
+        Snackbar.make(coordinatorLayout, R.string.powered_by_NewsApi, Snackbar.LENGTH_LONG)
+                .setAction(R.string.visit, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        loadNewsApiWebsite();
+                    }
+                }).show();
+    }
+
+    private void loadNewsApiWebsite() {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.newsApi_uri))));
+    }
 }
+
